4a55fa4cb342d9e766b34283c785de00b174de57
Felipe Bueno <felipebuenonunes16@gmail.com>
2022-10-08 18:27:55 -0300

    sera

--- docs/updateWiki/main.sh
+++ docs/updateWiki/main.sh
@@ -5,7 +5,8 @@ if [ -z "$TOKEN" ]; then
     exit 1
 fi
 
+echo $TOKEN
 # Clone repository
-git clone "https://$GITHUB_ACTOR:$TOKEN@github.com/$GITHUB_REPOSITORY"
+#git clone "https://$GITHUB_ACTOR:$TOKEN@github.com/$GITHUB_REPOSITORY"
 # cd "$GITHUB_REPOSITORY"
 pwd
