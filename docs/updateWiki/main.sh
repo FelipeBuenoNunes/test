3d17375185e9605c69a29daf25999d2fa5ef8c32
Felipe Bueno <felipebuenonunes16@gmail.com>
2022-10-08 19:13:46 -0300

    test name

--- docs/updateWiki/main.sh
+++ docs/updateWiki/main.sh
@@ -5,11 +5,14 @@ if [ -z "$TOKEN" ]; then
     exit 1
 fi
 
+TEMP_REPO_NAME="wiki-repo"
+
 echo $TOKEN
 # Clone repository
 #git clone "https://$GITHUB_ACTOR:$TOKEN@github.com/$GITHUB_REPOSITORY"
 #cd "$GITHUB_REPOSITORY"
-echo "https://$GITHUB_ACTOR:@github.com/$GITHUB_REPOSITORY.wiki.git"
-git clone "https://$GITHUB_ACTOR:$TOKEN@github.com/$GITHUB_REPOSITORY.wiki.git"
+git clone "https://$GITHUB_ACTOR:$TOKEN@github.com/$GITHUB_REPOSITORY.wiki.git" "$TEMP_REPO_NAME"
+ls
+cd $TEMP_REPO_NAME
 ls
 #git commit -m "$message" && git push "https://$GITHUB_ACTOR:$GH_TOKEN@github.com/$GITHUB_REPOSITORY.wiki.git"
