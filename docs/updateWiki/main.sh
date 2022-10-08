ebd8656211563e9309e72ce2bcfc7ae51c252d16
Felipe Bueno <felipebuenonunes16@gmail.com>
2022-10-08 18:33:36 -0300

    uhul

--- docs/updateWiki/main.sh
+++ docs/updateWiki/main.sh
@@ -7,6 +7,6 @@ fi
 
 echo $TOKEN
 # Clone repository
-#git clone "https://$GITHUB_ACTOR:$TOKEN@github.com/$GITHUB_REPOSITORY"
-# cd "$GITHUB_REPOSITORY"
+git clone "https://$GITHUB_ACTOR:$TOKEN@github.com/$GITHUB_REPOSITORY"
+cd "$GITHUB_REPOSITORY"
 pwd
