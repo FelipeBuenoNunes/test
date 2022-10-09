bac5713f997ce151aae76c48d47b7460cc7611b8
Felipe Bueno <felipebuenonunes16@gmail.com>
2022-10-09 17:23:22 -0300

    boa

--- docs/updateWiki/test.sh
+++ docs/updateWiki/test.sh
@@ -1,21 +1,37 @@
 #!/bin/bash
 
 PATH_FILE='/docs/diagram'
-FILES_PUML=$(ls .$PATH_FILE -t -U | grep .puml$)
+FILES_PUML=$(ls .$PATH_FILE -t -U | grep '^[a-z_]\+\.puml$')
 
 function getAndSaveImg() {
     echo $1
 }
 
-for i in $FILES_PUML; do
-    getAndSaveImg i
+for file_name in $FILES_PUML; do
+    CONTENT_FILE=$(<".$PATH_FILE/$file_name") # get file
+    CONTENT_WITH_THEME=${sed s/^\'include_theme_here$/https://raw.githubusercontent.com/FelipeBuenoNunes/test/main/docs/diagram/puml-theme-sla.puml/ first_diagram.puml}
+    HEX_TEXT=$(echo -n "$CONTENT_FILE" | od -A n -t x1) # text to hex
+    HEX_TEXT=${HEX_TEXT//[^a-z^0-9]/} #remove spaces
+    NEW_FILE_NAME=${file_name//.puml/.svg}
+    curl www.plantuml.com/plantuml/svg/~h$HEX_TEXT --output .$PATH_FILE/$NEW_FILE_NAME
 done
 
-for i in $thefile; do
-    name=`expr match "$i" '\([a-z_]*\)'` #
-    name=${name//_/ }
-    name=${name^}
-    echo $name
-done
+# get configs git
+author=`git log -1 --format="%an"`
+email=`git log -1 --format="%ae"`
+message=`git log -1 --format="%s"`
 
-removeAllSvg
+# set configs git
+git config --global user.email "$email"
+git config --global user.name "$author"
+
+
+git add .
+git commit -m "boa"
+git push
+# for i in $thefile; do
+#     name=`expr match "$i" '\([a-z_]*\)'` #
+#     name=${name//_/ }
+#     name=${name^}
+#     echo $name
+# done
