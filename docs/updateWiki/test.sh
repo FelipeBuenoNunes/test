8eed1d7b940fcc5ccf1cc8c5d7ed7671cd323e6e
Felipe Bueno <felipebuenonunes16@gmail.com>
2022-10-09 18:01:44 -0300

    test

--- docs/updateWiki/test.sh
+++ docs/updateWiki/test.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 
 PATH_FILE='/docs/diagram'
-FILES_PUML=$(ls .$PATH_FILE -t -U | grep '^[a-z_]\+\.puml$')
+if [[ $PATH_FILE =~ ^[^.] ]] 
+then
+    PATH_FILE=".$PATH_FILE"
+fi
+
+
+FILES_PUML=$(ls $PATH_FILE -t -U | grep '^[a-z_]\+\.puml$')
 
 function getAndSaveImg() {
     echo $1
 }
 
 for file_name in $FILES_PUML; do
-    CONTENT_FILE=$(<".$PATH_FILE/$file_name") # get file
-    CONTENT_WITH_THEME=${sed s/^\'include_theme_here$/https://raw.githubusercontent.com/FelipeBuenoNunes/test/main/docs/diagram/puml-theme-sla.puml/ first_diagram.puml}
-    HEX_TEXT=$(echo -n "$CONTENT_FILE" | od -A n -t x1) # text to hex
+    CONTENT_FILE=$(<"$PATH_FILE/$file_name") # get file
+
+    THEME_LINK="https://raw.githubusercontent.com/FelipeBuenoNunes/test/main/docs/diagram/puml-theme-sla.puml"
+    
+    #replace ('include_theme_here) for the theme link
+    CONTENT_WITH_THEME=$(sed "s,'include_theme_here,!include $THEME_LINK,g" $PATH_FILE/$file_name)
+    
+    HEX_TEXT=$(echo -n "$CONTENT_WITH_THEME" | od -A n -t x1) # text to hex
     HEX_TEXT=${HEX_TEXT//[^a-z^0-9]/} #remove spaces
-    NEW_FILE_NAME=${file_name//.puml/.svg}
-    curl www.plantuml.com/plantuml/svg/~h$HEX_TEXT --output .$PATH_FILE/$NEW_FILE_NAME
+    
+    NEW_FILE_NAME=${file_name//.puml/.png} 
+    
+    curl www.plantuml.com/plantuml/png/~h$HEX_TEXT --output $PATH_FILE/$NEW_FILE_NAME
 done
 
 # get configs git
